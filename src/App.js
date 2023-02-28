99036b6862c49ce9fde430f498c2b79b2f81ac49
dyoo47 <34610019+dyoo47@users.noreply.github.com>
2023-02-28 13:41:50 -0800

    Stylized navbar

--- src/App.js
+++ src/App.js
@@ -5,29 +5,48 @@ import About from "./About";
 import ViewSample from "./ViewSample";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import {
+  Collapse,
+  Container,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="About">About</Link>
-          </li>
-          <li>
-            <Link to="sample">View Sample</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <Navbar className="mx-5">
+        <NavbarBrand href="/">Energizers</NavbarBrand>
+        <Nav className="me-auto">
+          <NavItem>
+            <NavLink tag={Link} to="/">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/about">
+              About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/sample">
+              View Sample
+            </NavLink>
+          </NavItem>
+        </Nav>
+
+        <NavbarText className="text-end">SYN100</NavbarText>
+      </Navbar>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sample" element={<ViewSample />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>
 );
