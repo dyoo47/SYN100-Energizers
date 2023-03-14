b0f74bf6399f3eee66359032bf39957edb15f47e
World1014 <87560946+World1014@users.noreply.github.com>
2023-03-14 15:35:36 -0700

    updated About page, added css and added image for page to fill up empty space

--- src/About.js
+++ src/About.js
@@ -1,5 +1,41 @@
+import "./About.css"
 const About = () => {
-    return <h1>About</h1>;
+    return( 
+     <>
+    <h1 className="title">About</h1>
+    <span className="page">
+      <div className="image">
+        <img src="carbon-neutral-lead.jpg" alt="solar_panels" height={600} width={1100}/>
+        <small className="small">Photo by Erik Jepsen/UC San Diego Publications</small>
+      </div>
+      <span className="paragraph">
+      Hello! We are the Energizers, a group of students studying at UC San Diego that are passionate about
+      helping to teaching others about potential solutions to the climate crisis. This is why we have designed
+      this website as a project for the Synthesis program at our school. Our goal is to help spread awareness 
+      of the severity of pollution and climate change in the energy sector, and to educate people on the pros
+      and cons of each popular type of energy in use today, so people can make informed and educated decisions 
+      about their potential use in the future. We aim for this information/curriculum to be used by teachers to allow 
+      for them to more easily teach their students about the climate crisis using factual evidence from trusted sources,
+      but not requiring as much time as it might take to individually analyze each of the sources. We also hope that this information 
+      that we are sharing is able to inspire others to become interested in climate change so that we are able to reduce 
+      its disasterous affects on our planet, and ultimately help those that are being dispropotionately affected by the 
+      effects climate change has had and continues to have on our planet!
+      <br/> <br/>
+
+        Our sources we used to make our website/curriculum are: <br/>
+        <a href="url">https://www.nrc.gov/waste.html</a> <br/>
+        <a href="url">https://www.world-nuclear.org/information-library/nuclear-fuel-cycle/nuclear-wastes/radioactive-waste-management.aspx</a> <br/>
+        <a href="url">https://www.nrc.gov/waste/spent-fuel-storage.html</a> <br/>
+        <a href="url">https://www.nei.org/fundamentals/nuclear-fuel</a> <br/>
+        <a href="url">https://www.world-nuclear.org/information-library/nuclear-fuel-cycle/nuclear-power-reactors.aspx</a> <br/>
+        <a href="url">https://www.iaea.org/topics/radioactive-waste-management</a> <br/>
+        <a href="url">https://www.world-nuclear.org/information-library/economic-aspects/economics-of-nuclear-power.aspx</a> <br/>
+        <a href="url">https://ucsandiego.photoshelter.com/galleries/C0000GXkNjxnr5mo/G0000DBPQ.N_ezgc/Places-and-Landmarks</a> <br/>
+
+      </span>
+    </span>
+    </>
+    );
   };
   
   export default About;
